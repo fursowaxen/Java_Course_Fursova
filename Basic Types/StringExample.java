1903179483fd00cebf30bb66ce362d03cb040b50
fursowaxen <144027354+fursowaxen@users.noreply.github.com>
2026-05-30 23:43:36 +0300

    Практика 4

---
+++ Basic Types/StringExample.java
@@ -0,0 +1,16 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String s1 = "Hello" + " World";
+
+        String s2 = "Number: " + 42;
+
+        String s3 = "Pi = " + 3.14;
+
+        String s4 = "Is true? " + true;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
